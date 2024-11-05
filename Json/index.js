db154eb786b9bb7574e62968cd7f6a29cc040352
GabriellaFS <gabriella.feitosadesalive@hotmail.com>
2024-11-05 17:17:58 -0300

    testando vetores

---
+++ Json/index.js
@@ -0,0 +1,4 @@
+// JSON - JavaScript object Notation
+// chave e valor com o objetivo de transferir dados
+let name = "Gabriella"
+let age = 28 
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log(" Olá! Estamos começando mais uma sessão de estudos...");
+// variável, função e vetores 
+
+let name = ["João", "Willian", "Beny"]
+let age = [26, 28, 25]
+let products = [" blusa", "calça", "tênis"]
+let produtsValues = [ 26.99, 129.99, 299.99]
+
+generateInvoice(name, age, products, produtsValues  )
+
+function generateInvoice(name, age, products, produtsValues){
+    console.log(`O comprador é ${name[0]}`);
+    console.log("A idade é " + age[0]);
+    console.log("O produto é " + products[0]);
+    console.log("O valor é " + produtsValues[0]);
+    console.log(`O comprador é ${name[1]}`);
+    console.log("A idade é " + age[1]);
+    console.log("O produto é " + products[1]);
+    console.log("O valor é " + produtsValues[1]);
+    console.log(`O comprador é ${name[2]}` );
+    console.log("A idade é " + age[2]);
+    console.log("O produto é " + products[2]);
+    console.log("O valor é " + produtsValues[2]);
+}
