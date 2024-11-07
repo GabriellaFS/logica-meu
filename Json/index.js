d28f503fb736b062b1bbd5ff77f227b9e3646711
GabriellaFS <gabriella.feitosadesalive@hotmail.com>
2024-11-07 14:11:36 -0300

    estudando Json

--- Json/index.js
+++ Json/index.js
@@ -1,4 +1,18 @@
-// JSON - JavaScript object Notation
-// chave e valor com o objetivo de transferir dados
-let name = "Gabriella"
-let age = 28 
+let invoice ={
+    name: "Fernanda",
+    age: 18,
+    products:{
+      0: ["caderno", 76.99],
+      1: ["caneta", 5.99],
+      2: ["lápis", 1.99]
+    },
+    taxes: 95.90
+}
+
+ console.log(`A compradora é ${invoice.name}`);
+ console.log(`A idade é ${invoice.age}`);
+
+for ( let index in invoice.products){
+ let [ productName, productPrice] = invoice.products[index]
+ console.log(`Produto ${productName}: R$ ${productPrice}`);
+}
