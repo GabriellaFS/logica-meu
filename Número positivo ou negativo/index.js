0ca85ef0a5bea38949040172608992dd94c895e2
GabriellaFS <gabriella.feitosadesalive@hotmail.com>
2024-11-21 22:06:27 -0300

    número positivo ou negativo

---
+++ Número positivo ou negativo/index.js
@@ -0,0 +1,11 @@
+function verificarNum(num){
+num = -3
+if (num > 0 ){
+    console.log(" O número é positivo");
+}else if( num < 0 ){
+    console.log(" O número é negativo");
+}else{
+    console.log(" O número é Zero")
+}
+}
+verificarNum()
